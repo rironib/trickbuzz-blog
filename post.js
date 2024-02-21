c39da511ab4ebf449417597120f6d9f07a301b7e
Razibul Islam <dev.ronib@gmail.com>
2024-02-21 08:49:07 +0600

    Initial commit

---
+++ post.js
@@ -0,0 +1,241 @@
+// 1
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231201/">
+  <div class="postCard">
+      <img src="/images/thumbnail-1.webp" alt="thumbnail-1">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>01</span>
+              Mastering Efficiency and Simplicity with Python Programming
+          </h2>
+          <p class="postExcerpt">
+              In the ever-evolving landscape of programming languages, Python stands out as a versatile and powerful tool that champions both efficiency and simplicity. Whether you're a seasoned developer or just starting your coding journey, mastering Python can unlock a world of possibilities. In this article, we will explore how Python's unique features...
+          </p>
+      </div>
+  </div>
+</a>          `;
+
+  var postContainer = document.getElementById("post-1");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 2
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231202/">
+  <div class="postCard">
+      <img src="/images/thumbnail-2.webp" alt="thumbnail-2">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>02</span>
+              Unleashing Dynamic Web Experiences with JavaScript Brilliance
+          </h2>
+          <p class="postExcerpt">
+              In the ever-evolving landscape of web development, the role of JavaScript cannot be overstated. Its dynamic nature empowers developers to create interactive and engaging web experiences that captivate users. As the backbone of modern web development, JavaScript brings brilliance to the forefront, allowing for the creation...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-2");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 3
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231203/">
+    <div class="postCard">
+      <img src="/images/thumbnail-3.webp" alt="thumbnail-3">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>03</span>
+              Building Robust and Scalable Applications with Java Mastery
+          </h2>
+          <p class="postExcerpt">
+              In the fast-paced world of software development, creating applications that are not only robust but also scalable is imperative. Java, with its time-tested capabilities, has emerged as a powerhouse for building such applications. Mastering Java is not just about writing code; it's about harnessing the language's features to create applications that...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-3");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 4
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231204/">
+  <div class="postCard">
+      <img src="/images/thumbnail-4.webp" alt="thumbnail-4">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>04</span>
+              Crafting Powerful and Elegant Solutions with C# Expertise
+          </h2>
+          <p class="postExcerpt">
+              In the dynamic landscape of software development, crafting solutions that seamlessly blend power and elegance is an art. Among the myriad programming languages available, C# stands out as a versatile and robust choice. With its roots in the Microsoft ecosystem, C# has evolved into a language that empowers developers to create sophisticated...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-4");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 5
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231205/">
+  <div class="postCard">
+      <img src="/images/thumbnail-5.webp" alt="thumbnail-5">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>05</span>
+              Optimizing Performance and Precision with C++ Programming
+          </h2>
+          <p class="postExcerpt">
+              In the fast-paced world of software development, achieving optimal performance and precision is crucial. One programming language that stands out in this regard is C++. Known for its efficiency and versatility, C++ provides developers with the tools to fine-tune their code for maximum speed and accuracy. In this article, we'll delve into the...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-5");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 6
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231206/">
+  <div class="postCard">
+      <img src="/images/thumbnail-6.webp" alt="thumbnail-6">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>06</span>
+              Elevating Web Development to New Heights with PHP Prowess
+          </h2>
+          <p class="postExcerpt">
+              In the ever-evolving landscape of web development, staying at the forefront of technology is imperative. One programming language that has consistently proven its mettle in crafting robust and dynamic web applications is PHP. Renowned for its versatility and scalability, PHP has played a pivotal role in shaping the digital realm...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-6");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 7
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231207/">
+  <div class="postCard">
+      <img src="/images/thumbnail-7.webp" alt="thumbnail-7">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>07</span>
+              Enhancing JavaScript Development with TypeScript Ingenuity
+          </h2>
+          <p class="postExcerpt">
+              JavaScript has long been the go-to language for building dynamic and interactive web applications. However, as projects grow in complexity, developers often find themselves grappling with challenges related to maintainability, scalability, and robustness. This is where TypeScript steps in as a powerful solution to elevate JavaScript development...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-7");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 8
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231208/">
+  <div class="postCard">
+      <img src="/images/thumbnail-8.webp" alt="thumbnail-8">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>08</span>
+              Swiftly Crafting iOS Excellence in App Development
+          </h2>
+          <p class="postExcerpt">
+              In the dynamic realm of mobile app development, Swift has emerged as a powerful and efficient programming language, particularly for iOS applications. Swift's ability to seamlessly blend performance with readability has made it the language of choice for developers aiming to create top-notch iOS apps...
+          </p>
+      </div>
+  </div>
+</a>            `;
+
+  var postContainer = document.getElementById("post-8");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 9
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231209/">
+  <div class="postCard">
+      <img src="/images/thumbnail-9.webp" alt="thumbnail-9">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>09</span>
+              Unleashing Creativity and Productivity through Ruby Magic
+          </h2>
+          <p class="postExcerpt">
+              In the dynamic realm of programming languages, Ruby stands out as a versatile and powerful tool that goes beyond its conventional use. Beyond its syntax and object-oriented nature, Ruby possesses a certain magic that can unlock new levels of creativity and productivity for developers. This article delves into the enchanting world of Ruby...
+          </p>
+      </div>
+  </div>
+</a>
+            `;
+
+  var postContainer = document.getElementById("post-9");
+
+  postContainer.appendChild(postCardContainer);
+});
+
+// 10
+document.addEventListener("DOMContentLoaded", function () {
+  var postCardContainer = document.createElement("div");
+  postCardContainer.innerHTML = `
+  <a href="/post/20231210/">
+  <div class="postCard">
+      <img src="/images/thumbnail-10.webp" alt="thumbnail-10">
+      <div class="postInfo">
+          <h2 class="postName">
+              <span>10</span>
+              Efficient and Scalable Development with Go Programming Mastery
+          </h2>
+          <p class="postExcerpt">
+              In the dynamic realm of software development, mastering a language that combines efficiency and scalability is the key to success. Go, also known as Golang, has emerged as a powerhouse, offering developers a robust platform to build applications that are not only efficient but also highly scalable. In this article, we delve into the world of Go...
+          </p>
+      </div>
+  </div>
+</a>
+            `;
+
+  var postContainer = document.getElementById("post-10");
+
+  postContainer.appendChild(postCardContainer);
+});
